a8087a0ee56371617199b54f40a144fea2c9cb89
judithhinlung <judithhin@gmail.com>
2024-08-21 15:31:52 -0700

    Authentication

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'Buddy' });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+            keys: [keys.cookieKey]
+    })
+)
+
+require('./routes/authRoutes')(app);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+console.log("Server is up on port ", PORT);
