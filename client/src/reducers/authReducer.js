49904b1509d8aa6fc3386e6a9eae676834114930
judithhinlung <judithhin@gmail.com>
2024-09-16 14:49:28 -0700

    Finished header setup

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,8 @@
-function authReducer(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+function authReducer(state = null, action) {
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
     default:
       return state;
   }
