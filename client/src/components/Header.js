45e239f9433734a08451f746444c1fd41ef53776
judithhinlung <judithhin@gmail.com>
2024-10-02 13:04:40 -0700

    Finished handling billing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,8 +12,11 @@ class Header extends Component {
         return <li><a href="/auth/google">Login With Google</a></li>;
       default:
         return [
-          <li><Payments /></li>,
-          <li><a href="/api/logout">Logout</a></li>
+          <li key="1"><Payments /></li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+          Credits: {this.props.auth.credits}
+        </li>,
+          <li key="2"><a href="/api/logout">Logout</a></li>
         ];
     }
   }
